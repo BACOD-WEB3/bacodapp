fbb599ea85941588b31a79dd1e0e055c1ed1fe17
Dale Watson <dev@dellwatson.com>
2022-11-08 21:41:35 +0700

    feat: add tailwind, update package

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  purge: ["./src/pages/**/*", "./src/components/**/*"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
